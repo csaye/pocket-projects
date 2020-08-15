ad56b4bf567e7b7021ef685dbbe92f4c211aaa25
Cooper Saye <coopersaye@gmail.com>
2020-08-15 15:33:15 -0700

    Import art.

---
+++ Assets/PocketProjects/Projects/BulletHell/Scripts/FloorGenerator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace PocketProjects.BulletHell
+{
+    [RequireComponent(typeof(Tilemap))]
+    public class FloorGenerator : MonoBehaviour
+    {
+        [Header("Attributes")]
+        [SerializeField] private Vector2Int floorSize = new Vector2Int();
+
+        private Tilemap tilemap;
+
+        private void Start()
+        {
+            tilemap = GetComponent<Tilemap>();
+
+            GenerateFloor();
+        }
+
+        private void GenerateFloor()
+        {
+            Debug.Log(floorSize);
+        }
+    }
+}
