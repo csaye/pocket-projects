f5c9f2e89d8766bd28ef6ebcf2c71d003910a3d2
Cooper Saye <coopersaye@gmail.com>
2020-08-15 20:51:48 -0700

    Set up basic bullet and bullet spawner structures.

---
+++ Assets/PocketProjects/Projects/BulletHell/Scripts/Bullet.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace PocketProjects.BulletHell
+{
+    public class Bullet : MonoBehaviour
+    {
+        private float movementSpeed;
+        private Vector2 movementDirection;
+
+        private void Update()
+        {
+            transform.position = transform.position + movementDirection * movementSpeed;
+        }
+
+        public void ActivateBullet()
+        {
+            transform.position = GetRandomPosition();
+            movementSpeed = GetRandomSpeed();
+        }
+
+        private Vector2 GetRandomPosition()
+        {
+
+            movementDirection = 
+        }
+
+        private float GetRandomSpeed()
+        {
+            
+        }
+
+        private void DeactivateBullet()
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void OnEnable()
+        {
+            ActivateBullet();
+        }
+
+        private void OnBecameInvisible()
+        {
+            DeactivateBullet();
+        }
+    }
+}
---
+++ Assets/PocketProjects/Projects/BulletHell/Scripts/BulletSpawner.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+namespace PocketProjects.BulletHell
+{
+    public class BulletSpawner : MonoBehaviour
+    {
+        [Header("Attributes")]
+        [SerializeField] private int bulletPoolSize = 0;
+        [SerializeField] private float bulletSpawnTime = 0;
+
+        [Header("References")]
+        [SerializeField] private GameObject[] bulletPrefabs = new GameObject[6];
+
+        private GameObject[] bullets = new GameObject[16];
+
+        private float lastSpawnTime;
+
+        private void Start()
+        {
+            InstantiateBullets();
+        }
+
+        private void InstantiateBullets()
+        {
+            for (int i = 0; i < bulletPoolSize; i++)
+            {
+                bullets[i] = Instantiate(RandomBullet(), Vector2.zero, Quaternion.identity, transform);
+                bullets[i].SetActive(false);
+            }
+        }
+
+        private GameObject RandomBullet()
+        {
+            return bulletPrefabs[Random.Range(0, bulletPrefabs.Length)];
+        }
+
+        private void Update()
+        {
+            if (Time.time - lastSpawnTime > bulletSpawnTime)
+            {
+                lastSpawnTime = Time.time;
+                SpawnBullet();
+            }
+        }
+
+        private void SpawnBullet()
+        {
+            for (int i = 0; i < bullets.Length; i++)
+            {
+                // If bullet not active
+                if (!bullets[i].activeSelf)
+                {
+                    InitializeBullet(i);
+                    return;
+                }
+            }
+
+            Debug.LogError("Bullet call exceeds pool capacity.");
+        }
+
+        private void InitializeBullet(int index)
+        {
+            bullets[index].SetActive(true);
+        }
+
+        private Vector2 RandomPosition()
+        {
+            return Vector2.zero;
+        }
+    }
+}
