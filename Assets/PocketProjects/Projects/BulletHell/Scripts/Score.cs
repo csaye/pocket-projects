86369d1ae981b6003ea6d581ab57edaa909f598f
Cooper Saye <coopersaye@gmail.com>
2020-08-16 13:11:36 -0700

    Set up basic scoring system.

---
+++ Assets/PocketProjects/Projects/BulletHell/Scripts/Score.cs
@@ -0,0 +1,50 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace PocketProjects.BulletHell
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class Score : MonoBehaviour
+    {
+        [Header("Attributes")]
+        [SerializeField] private float scoreSpeed = 0;
+
+        private TextMeshProUGUI textField;
+
+        private float lastScoreTime;
+
+        private int score;
+
+        private void Start()
+        {
+            textField = GetComponent<TextMeshProUGUI>();
+        }
+
+        private void Update()
+        {
+            if (Time.time - lastScoreTime > scoreSpeed)
+            {
+                IncrementScore();
+            }
+        }
+
+        private void IncrementScore()
+        {
+            score++;
+
+            PlayerPrefs.SetInt("BulletHellScore", score);
+
+            if (score > PlayerPrefs.GetInt("BulletHellHighScore", 0))
+            {
+                PlayerPrefs.
+            }
+
+            UpdateTextField();
+        }
+
+        private void UpdateTextField()
+        {
+            textField.text = $"Score: {score}";
+        }
+    }
+}
---
+++ Assets/PocketProjects/Projects/BulletHell/Scripts/ScoreText.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace PocketProjects.BulletHell
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class ScoreText : MonoBehaviour
+    {
+        private TextMeshProUGUI textField;
+
+        private void Start()
+        {
+            textField = GetComponent<TextMeshProUGUI>();
+
+            SetTextField();
+        }
+
+        private void SetTextField()
+        {
+            int score = PlayerPrefs.GetInt("BulletHellScore", 0);
+            int highScore = PlayerPrefs.GetInt("BulletHellHighScore", 0);
+
+            textField.text = $"Score: {score}\nHigh Score: {highScore}"
+        }
+    }
+}
