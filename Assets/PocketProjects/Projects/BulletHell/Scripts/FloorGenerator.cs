64184d3224a50cc6399ec2e69da64cd4049c94cc
Cooper Saye <coopersaye@gmail.com>
2020-08-15 17:40:03 -0700

    Redo floor gbase generation system.

--- Assets/PocketProjects/Projects/BulletHell/Scripts/FloorGenerator.cs
+++ Assets/PocketProjects/Projects/BulletHell/Scripts/FloorGenerator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Tilemaps;
 
 namespace PocketProjects.BulletHell
@@ -7,32 +8,57 @@ namespace PocketProjects.BulletHell
     public class FloorGenerator : MonoBehaviour
     {
         [Header("Attributes")]
+        [SerializeField] private Vector2Int floorSize = new Vector2Int();
         [SerializeField] private Vector2Int baseSize = new Vector2Int();
-        // [SerializeField] private Vector2Int floorSize = new Vector2Int();
 
         [Header("References")]
         [SerializeField] private RuleTile floorTile = null;
 
         private Tilemap tilemap;
 
+        private BoundsInt floorBounds;
+        private BoundsInt baseBounds;
+
         private void Start()
         {
             tilemap = GetComponent<Tilemap>();
+
+            // Get floor and base bounds
+            Vector3Int floorOrigin = new Vector3Int(-floorSize.x / 2, -floorSize.y / 2, 0);
+            Vector3Int baseOrigin = new Vector3Int(-baseSize.x / 2, -baseSize.y / 2, 0);
+
+            Vector3Int floorBoundsSize = new Vector3Int(floorSize.x, floorSize.y, 1);
+            Vector3Int baseBoundsSize = new Vector3Int(baseSize.x, baseSize.y, 1);
+
+            floorBounds = new BoundsInt(floorOrigin, floorBoundsSize);
+            baseBounds = new BoundsInt(baseOrigin, baseBoundsSize);
 
             GenerateFloor();
         }
 
         private void GenerateFloor()
         {
-            // Generate base
-            for (int x = -baseSize.x / 2; x < baseSize.x / 2; x++)
-            {
-                for (int y = -baseSize.y / 2; y < baseSize.y / 2; y++)
-                {
-                    Vector3Int position = new Vector3Int(x, y, 0);
+            List<Vector3Int> tilePositions = new List<Vector3Int>();
 
-                    tilemap.SetTile(position, floorTile);
+            // Generate floor
+            foreach (Vector3Int position in floorBounds.allPositionsWithin)
+            {
+                // If part of base
+                if (baseBounds.Contains(position))
+                {
+                    tilePositions.Add(position);
+                    continue;
                 }
+            }
+
+            CreateTiles(tilePositions);
+        }
+
+        private void CreateTiles(List<Vector3Int> tilePositions)
+        {
+            foreach (Vector3Int position in tilePositions)
+            {
+                tilemap.SetTile(position, floorTile);
             }
         }
     }
---
+++ Assets/PocketProjects/Scripts/Math.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace PocketProjects
+{
+    public class Math
+    {
+        public static bool Contains(BoundsInt bounds, Vector3Int position)
+        {
+            return bounds.min.x <= position.x && bounds.min.y <= position.y && bounds.max.x > position.x && bounds.max.y > position.y;
+        }
+    }
+}
