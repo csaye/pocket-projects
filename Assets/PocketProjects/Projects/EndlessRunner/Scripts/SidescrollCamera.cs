891a49d4f78c9c05c961d9281ab780cd40d6b54f
Cooper Saye <coopersaye@gmail.com>
2020-08-23 14:31:10 -0700

    Implement sidescroll camera for endless runner.

---
+++ Assets/PocketProjects/Projects/EndlessRunner/Scripts/SidescrollCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace PocketProjects.EndlessRunner
+{
+    public class SidescrollCamera : MonoBehaviour
+    {
+        [Header("Attributes")]
+        [SerializeField] private float scrollSpeed = 0;
+
+        private float yPos;
+        private float zPos;
+
+        private void Start()
+        {
+            yPos = transform.position.y;
+            zPos = transform.position.z;
+        }
+
+        private void Update()
+        {
+            Scroll(scrollSpeed * Time.deltaTime);
+        }
+
+        private void Scroll(float scrollDistance)
+        {
+            transform.position = new Vector3(transform.position.x + scrollDistance, yPos, zPos);
+        }
+    }
+}
